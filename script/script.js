21f25b5fecbc0b3a4bcea06a26b3754717ecfc04
Mark Grian Tisoy <iamarkgrian@gmail.com>
2021-05-16 23:52:55 +0800

    Update 1

--- script/script.js
+++ script/script.js
@@ -1,11 +1,14 @@
 const hero = document.querySelector('.hero');
 const menuToggle = document.querySelector('.toggle');
 const closeToggle = document.querySelector('.toggle-close');
+const body = document.querySelector('body');
 
 menuToggle.addEventListener('click', () => {
     hero.classList.add('active');
+    body.style.overflow = 'scroll'
 });
 
 closeToggle.addEventListener('click', () => {
     hero.classList.remove('active');
+    body.style.overflow = 'hidden';
 });
